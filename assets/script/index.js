72e103052c420f9921a5d0b02a6110e810150b17
av6920337 <av6920337@gmail.com>
2024-06-28 15:24:27 +0200

    Added styles, script and completed html

---
+++ assets/script/index.js
@@ -0,0 +1,4 @@
+const inputName = document.getElementById('inputName');
+const inputAvatar = document.getElementById('inputAvatar');
+const inputComment = document.getElementById('inputComment');
+const addBtn = document.getElementById('addBtn');
