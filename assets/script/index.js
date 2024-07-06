6835c808b431539ac2c27b746921ccd7bf31a442
av6920337 <av6920337@gmail.com>
2024-07-06 18:49:48 +0200

    the work 12 and 13 are completed

--- assets/script/index.js
+++ assets/script/index.js
@@ -4,20 +4,38 @@ const inputComment = document.getElementById('inputComment');
 const addBtn = document.getElementById('addBtn');
 const blockForChat = document.querySelector('.blockForChat');
 
+const check = document.querySelector('.checkbox');
+const linksAvatar = [
+    "https://avatars.mds.yandex.net/i?id=6b8cd7036a69113e4162d4f9af42630ed533f96caabbaf71-12496215-images-thumbs&n=13",
+    "https://avatars.mds.yandex.net/i?id=8c2e525475c5328f1557070f13f274d964e59e74735ee65e-11540573-images-thumbs&n=13",
+    "https://avatars.mds.yandex.net/i?id=12bc78490d7b63348c7485e4a6ad908a658e13feabb77ba1-10231558-images-thumbs&n=13",
+    "https://avatars.mds.yandex.net/i?id=08f0cfb2065996f3aa866ac2e1ec47825407cb41ce8b5b08-5339410-images-thumbs&n=13",
+    "https://avatars.mds.yandex.net/i?id=38583a6bbc477ca067f4bccc0a2767538c9e5c2d4dffce7c-12729315-images-thumbs&n=13",
+];
 
 function validateUserName(){
+    let result = check.checked ? "username" : false;
     const nameValue = inputName.value; 
-
-    let correctValueName = nameValue.trim();
-    correctValueName = correctValueName[0].toUpperCase() + correctValueName.slice(1).toLowerCase();
     inputName.value='';
-    return correctValueName;
+    if (!result) {
+        let correctValueName = nameValue.trim();
+        correctValueName = correctValueName[0].toUpperCase() + correctValueName.slice(1).toLowerCase();
+        return correctValueName;
+    }
+    else{
+        return result;
+    }
 }
 
 function workWithUserLink(){
     const avatarUser = inputAvatar.value;
     inputAvatar.value='';
-    return avatarUser;
+    // return avatarUser;
+
+    if(avatarUser === ''){
+        return linksAvatar[Math.floor(Math.random() * 5)];
+    }
+    else{return avatarUser;}
 }
 
 function checkSpam(){
@@ -33,6 +51,11 @@ function checkSpam(){
     }
 }
 
+function getTimeWritingComment(){
+    const date = new Date();
+    return date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+}
+
 addBtn.addEventListener('click', function(event){
     event.preventDefault();
     //функция работы с пользовательским именем
@@ -41,16 +64,22 @@ addBtn.addEventListener('click', function(event){
     //функция работы с пользовательским аватаром
     const validatedLink = workWithUserLink();
 
-    //функция работы с пользовательским comment
+    //функция работы с пользовательским commentariem
     const validatedComment = checkSpam();
 
-        const commentWrapper =`
+    const dateTime = getTimeWritingComment();
+
+    const commentWrapper =`
         <div class='card'>
-        <img src="${validatedLink}" alt="Avatar" class="userAvatar">
-                    <div class="wrapper">
-                        <p class="userName">${validatedName}</p>
-                    <p class="userComment">${validatedComment}</p>
-                    </div></div>
+            <img src="${validatedLink}" alt="Avatar" class="userAvatar">
+                        <div class="wrapper">
+                            <div class="wrapper_2">
+                            <p class="userName">${validatedName}</p>
+                            <span class="timeComment">${dateTime}</span>
+                            </div>
+                            <p class="userComment">${validatedComment}</p>
+                        </div>
+        </div>
         `;
         blockForChat.innerHTML += commentWrapper;
     
