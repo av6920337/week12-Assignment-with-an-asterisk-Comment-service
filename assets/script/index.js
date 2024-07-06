9e1dd9bfacd7bffccbedbf0640aa7df0e3e9934f
av6920337 <av6920337@gmail.com>
2024-07-06 16:32:13 +0200

    the work 12 is completed

--- assets/script/index.js
+++ assets/script/index.js
@@ -4,34 +4,55 @@ const inputComment = document.getElementById('inputComment');
 const addBtn = document.getElementById('addBtn');
 const blockForChat = document.querySelector('.blockForChat');
 
-addBtn.addEventListener('click', function(){
-    //функция работы с пользовательским именем
+
+function validateUserName(){
     const nameValue = inputName.value; 
+
     let correctValueName = nameValue.trim();
     correctValueName = correctValueName[0].toUpperCase() + correctValueName.slice(1).toLowerCase();
-    blockForChat.style.display = 'flex';
-    const userName = document.querySelector('.userName');
-    userName.textContent = correctValueName;
+    inputName.value='';
+    return correctValueName;
+}
+
+function workWithUserLink(){
+    const avatarUser = inputAvatar.value;
+    inputAvatar.value='';
+    return avatarUser;
+}
+
+function checkSpam(){
+    let commentUser = inputComment.value.trim();
+    let userCommentValueCorrect = commentUser.toLowerCase();
+    inputComment.value = '';
+
+    if(userCommentValueCorrect.includes('viagra') || userCommentValueCorrect.includes('xxx')) {
+        let modifiedComment = userCommentValueCorrect.replace(/viagra/g, '**').replace(/xxx/g, '**');
+        return modifiedComment;
+    } else {
+        return userCommentValueCorrect;
+    }
+}
+
+addBtn.addEventListener('click', function(event){
+    event.preventDefault();
+    //функция работы с пользовательским именем
+    const validatedName = validateUserName();
 
     //функция работы с пользовательским аватаром
-    const avatarUser = inputAvatar.value;
-    const userAvatar = document.querySelector('.userAvatar');
-    userAvatar.src=avatarUser;
-    
-    //функция работы с пользовательским comment
-    function checkSpam(){
-        let userCommentValue = inputComment.value;
-        const userComment = document.querySelector('.userComment');
-        userComment.textContent = userCommentValue;
+    const validatedLink = workWithUserLink();
 
-        if(userCommentValue.includes('viagra') || userCommentValue.includes('xxx')) {
-            let modifiedComment = userCommentValue.replaceAll('viagra', '***');
-            userComment.textContent = modifiedComment;
-        return modifiedComment;
-        }
-        else{
-            return userCommentValue;
-        }
-    }
-    checkSpam();
+    //функция работы с пользовательским comment
+    const validatedComment = checkSpam();
+
+        const commentWrapper =`
+        <div class='card'>
+        <img src="${validatedLink}" alt="Avatar" class="userAvatar">
+                    <div class="wrapper">
+                        <p class="userName">${validatedName}</p>
+                    <p class="userComment">${validatedComment}</p>
+                    </div></div>
+        `;
+        blockForChat.innerHTML += commentWrapper;
+    
 })
+
